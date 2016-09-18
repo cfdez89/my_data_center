bb6e28cf0179b7bf4a6bfe3fa4e4010c33fc85c2
Carlos Fernandez Jimenez <cfdez89@gmail.com>
2016-09-18 13:36:38 -0600

    Se corrigió la navegación

--- presentation/Controladores/ctlIndex.js
+++ presentation/Controladores/ctlIndex.js
@@ -13,55 +13,24 @@
 	'use strict';
 	angular
 		.module('myDataCenter')
-	    .controller('ctlIndex', [
-	    	'$scope',
-	    	'$location',
-	    	'svsSesion',
-	    	'svsNavegacion',
-	    	function($scope, $location, svsNavegacion){
+	    .controller('ctlIndex', ctlIndex);
+
+	function ctlIndex($scope, $location) {
 		
-				//$scope.navegacion = svsNavegacion.getNavegacion();
-				$scope.verMenu  = true;
-				//$scope.verMenu = false;
-				console.log($scope.verMenu);
-				$scope.tab = 1;
-				
-				$scope.selectTab = function(pTab){
-				//	this.tab = pTab;
-					console.log(this.tab);
-				    $location.path('/rankings').replace();
-				
-				};
-				
-				$scope.actualizarNavegacion = function(){
-					//svsNavegacion.setNavegacion(false);
-					///console.log(svsNavegacion.getNavegacion());
-					/*svsNavegacion.setNavegacion([
-						{nombre:"Jugadores", activo:1, url:"/"},
-						{nombre:"Rankings", activo:2, url:"/"},
-						{nombre:"Salir", activo:3, url:"/"}
-						]);*/
-				};
-		
-				$scope.isSelected = function(pTap){
-					return this.tab === pTap;
-				};
-				
-		        $scope.enviar= function(){
-		        	console.log($scope.navegacion);	
-					//$location.path('/rankings').replace();
-					
-		        };
-		        
-		
-				
-				
-				$scope.verJugadores = function(){
-					$location.path('/jugadores').replace();
-				};
-				
-				$scope.verRankings = function(){
-					$location.path('/rankings').replace();
-				};
-		}]);	
+		var vm = this;
+        /*
+		vm.verJugadores = function() {
+			$location.path('/jugadores').replace();
+		};
+		*/
+
+		var hideMenuBar = function() {
+			$('.navbar-collapse').click('li', function() {
+				$('.navbar-collapse').collapse('hide');
+			});
+		};	
+
+	    hideMenuBar();
+	};
+
 })();
