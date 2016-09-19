37f13c650e6366a967074c919fc95c20e8404e39
Carlos Fernandez Jimenez <cfdez89@gmail.com>
2016-09-18 22:14:27 -0600

    Corregido identacion

--- services/services.js
+++ services/services.js
@@ -9,44 +9,44 @@
 var nbaRepository = require('../repository/repository.js');
 
 exports.getAllSeasons = function(request, response) {
-	var data = nbaRepository.getAllPlayers();
-	response.status(200);
-	response.send(data);
+    var data = nbaRepository.getAllPlayers();
+    response.status(200);
+    response.send(data);
 };
 
 exports.getAllPlayers = function(request, response) {
     var data = nbaRepository.getAllPlayers();
-	response.status(200);
-	response.send(data);
+    response.status(200);
+    response.send(data);
 };
 
 exports.getPlayerById = function(request, response) {
-	var id = request.params.id;
+    var id = request.params.id;
     var data = nbaRepository.getPlayerById(id);
-	response.status(200);
-	response.send(data);
+    response.status(200);
+    response.send(data);
 };
 
 exports.getAllRegularSeasonPlayerById = function(request, response) {
     var data = nbaRepository.getAllPlayers();
-	response.status(200);
-	response.send(data);
+    response.status(200);
+    response.send(data);
 };
 
 exports.getAllPostSeasonPlayerById = function(request, response) {
     var data = nbaRepository.getAllPlayers();
-	response.status(200);
-	response.send(data);
+    response.status(200);
+    response.send(data);
 };
 
 exports.getAllGameStatisticsPlayerById = function(request, response) {
     var data = nbaRepository.getAllPlayers();
-	response.status(200);
+    response.status(200);
 	response.send(data);
 };
 
 exports.getAllSocialStatisticsPlayerById = function(request, response) {
     var data = nbaRepository.getAllPlayers();
-	response.status(200);
-	response.send(data);
+    response.status(200);
+    response.send(data);
 };
