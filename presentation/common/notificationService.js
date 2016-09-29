faeeee4fc29be776a00fb496d26f77532bedb779
Carlos Fernandez Jimenez <cfdez89@gmail.com>
2016-09-29 16:15:53 -0600

    Se agrega servicio para notificaciones genérico

---
+++ presentation/common/notificationService.js
@@ -0,0 +1,60 @@
+/*Tecnologico de Costa Rica
+ *Diseño de software
+ *X-NBA MyDataCenter
+ *Elaborado por Carlos Fernandez Jimenez
+ *Octubre 2015 
+ */
+ 
+ /*Servicio notificaciones
+ * Servicio para mostrar los mensajes de notificacion
+ */
+
+ (function() {
+    'use strict';
+
+    function notificationService(Flash) { 
+	
+        var showSuccess = function(pMessage) {
+
+        	var message = '<strong>Well done! </strong>';
+            return Flash.create('success', menssage + pMessage);
+        };
+
+        var showInfo = function(pMessage) {
+
+        	var message = '<strong>Important! </strong>';
+            return Flash.create('info', menssage + pMessage);  
+        };
+
+        var showWarning = function(pMessage) {
+
+        	var message = '<strong>Warning! </strong>';
+            return Flash.create('warning', menssage + pMessage);  
+        };
+
+        var showDanger = function(pMessage) {
+
+        	var message = '<strong>Error! </strong>';
+            return Flash.create('danger', menssage + pMessage);  
+        };
+
+        return {
+            successMessage: function(pMessage) {
+                return showSuccess(pMessage);
+            },
+            infoMessage: function(pMessage) {
+                return showInfo(pMessage);
+            },
+            warningMessage: function(pMessage) {
+                return showWarning(pMessage);
+            },
+            dangerMessage: function(pMessage) {
+                return showDanger(pMessage);
+            }
+        }
+    }; 
+    
+    angular
+        .module('myDataCenter')
+        .factory('notificationService', notificationService);
+})();
