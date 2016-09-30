e244891b76660ed60b44885425cf979f691a2f71
Carlos Fernandez Jimenez <cfdez89@gmail.com>
2016-09-30 01:11:16 -0600

    Se corrige el servicio para temporadas

---
+++ presentation/services/seasonsService.js
@@ -0,0 +1,41 @@
+/*Tecnologico de Costa Rica
+ *Diseño de software
+ *X-NBA MyDataCenter
+ *Elaborado por Carlos Fernandez Jimenez
+ *Octubre 2015 
+ */
+ 
+ /*Servicio temporadas
+ * Servicio para obtener todas las temporadas
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('myDataCenter')
+        .factory('seasonsService', seasonsService);
+
+    seasonsService.$inject = ['requestService'];
+
+    function seasonsService(requestService) {  
+        
+        var urlBase = 'http://localhost:3000/temporadas';
+        
+        var getSeasons = function() {
+            return  requestService.retrieve(urlBase).then(function(pResp) {
+                        return pResp.data; 
+                    },  
+                    function(pResp) {
+                        return pResp.data;   
+                    });
+        };
+   
+        return  {
+            getAllSeasons: function() {
+                return getSeasons();             
+            }
+        };
+    };
+    
+})();
