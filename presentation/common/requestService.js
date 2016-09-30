c0e7e73623da7c73666c49f262986f1e4406674a
Carlos Fernandez Jimenez <cfdez89@gmail.com>
2016-09-30 01:35:12 -0600

    Identación corregida

--- presentation/common/requestService.js
+++ presentation/common/requestService.js
@@ -19,34 +19,34 @@
 		return {
 		    create: function(pUrl, pData) {
 			    return $http({
-					        method: "POST",
-						    url: pUrl,
-						    data: pData,
-							headers: { 'Content-Type': 'application/json' }
-						});
-			},
-			retrieve: function(pUrl) {
-				return $http({
-					        method: "GET",
-						    url: pUrl,
-							headers: { 'Content-Type': 'application/json' }
-						});
-			},
-			update: function(pUrl, pData) {
-				return $http({
-					        method: "PUT",
-						    url: pUrl,
-						    data: pData,
-							headers: { 'Content-Type': 'application/json' }
-						});
-			},
-			delete: function(pUrl) {
-				return $http({
-					        method: "DELETE",
-						    url: pUrl,
-							headers: { 'Content-Type': 'application/json' }
-						});
-			}
+                    method: "POST",
+                    url: pUrl,
+                    data: pData,
+                    headers: { 'Content-Type': 'application/json' } 
+                });
+            },
+            retrieve: function(pUrl) {
+                return $http({
+                    method: "GET",
+                    url: pUrl,
+                    headers: { 'Content-Type': 'application/json' }
+                });
+            },
+            update: function(pUrl, pData) {
+                return $http({
+                    method: "PUT",
+                    url: pUrl,
+                    data: pData,
+                    headers: { 'Content-Type': 'application/json' }
+                });
+            },
+            delete: function(pUrl) {
+                return $http({
+                    method: "DELETE",
+                    url: pUrl,
+                    headers: { 'Content-Type': 'application/json' }
+                });
+            }
 		};   
 	};
 	
